a1058a802e2a5ed4161c0cdd50a3b863781bb1a5
kimcd <44451687+kimcd@users.noreply.github.com>
2022-05-09 16:33:06 -0400

    Create ASD.cpp

---
+++ src/ASD.cpp
@@ -0,0 +1,86 @@
+
+
+#include "ASD.h"
+
+#include <vector>
+using std::vector;
+
+#include <cmath>
+
+#include <iostream>
+using std::cerr;
+using std::endl;
+
+
+ASD::ASD(vector<unsigned int>& frequencies, vector<double>& asd)
+: frequencies_hz(validate_frequency_input(frequencies)),
+  asd_g2_hz(validate_asd_input(asd))
+{
+	g_rms = compute_rms();
+}
+
+
+vector<unsigned int> ASD::validate_frequency_input(const vector<unsigned int>& frequencies) const
+{
+	// check that frequencies are valid. how?
+	return frequencies;
+}
+
+
+vector<double> ASD::validate_asd_input(const vector<double>& asd) const
+{
+	// check that frequencies are valid. how?
+	return asd;
+}
+
+
+double ASD::compute_rms() const
+{
+	double area = 0.0;
+
+	for(size_t i = 1; i <= frequencies_hz.size(); ++i)
+	{
+
+		area += compute_area(
+				frequencies_hz[i - 1],
+				frequencies_hz[i],
+				asd_g2_hz[i-1],
+				asd_g2_hz[i]);
+	}
+
+	return sqrt(area);
+
+}
+
+
+double compute_area(const double f1, const double f2, const double asd1, const double asd2)
+{
+	const double EPS = 0.0000001f;
+
+	double db = compute_db(asd1, asd2);
+	double oct = compute_oct(f1, f2);
+	double m = db/oct;
+
+	double area;
+
+	// if
+	if(fabs( m - (-10 * log10(2)) ) < EPS)
+	{
+
+		area = asd1 * f1 * log(f2 / f1);
+
+	} else
+	{
+
+		cerr << (f2 - (f1 * pow( (f1 / f2), (m / (10 * log10(2))) ))) << endl;
+
+		area = 10 * log10(2) * ( asd2 / (10 * log10(2) + m) )
+				*
+				(f2 - (f1 * pow( (f1 / f2), (m / (10 * log10(2))) )) );
+
+	}
+
+	return area;
+
+}
+

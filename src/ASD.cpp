be0330d8867b2ed4d172ad884561e5dacdccec4a
kimcd1 <kimcd1@KIMCD1-WL3.dom1.jhuapl.edu>
2022-05-16 14:53:29 -0400

    just simple update

--- src/ASD.cpp
+++ src/ASD.cpp
@@ -17,7 +17,7 @@ const std::vector<double> ASD::GEVS_ASD = {0.026, 0.16, 0.16, .026};
 const std::vector<unsigned int> ASD::GEVS_FREQUENCIES = {20, 50, 800, 2000};
 const std::vector<double> ASD::GEVS_ASD = {0.026, 0.16, 0.16, .026};
 
-//Hello?
+
 ASD::ASD(vector<unsigned int> frequencies, vector<double> asd)
 : frequencies_hz(validate_frequency_input(frequencies)),
   asd_g2_hz(validate_asd_input(asd))
